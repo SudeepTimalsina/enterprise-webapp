7b743b056ac4754a5808dccea23b24bfeac9cfca
sudeeptimalsina <sudeeptimalsina18@gmail.com>
2026-08-05 13:09:04 +1000

    add link to project and landing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@enterprise-webapp/ui";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Welcome to the web App</h1>
-      <Button>Create Project</Button>
+      <Link href="/projects">
+        <Button>Create Project</Button>
+      </Link>
     </main>
   );
 }
