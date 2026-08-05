9c3954343a66be63b7ead917eb55e93260e7f81b
sudeeptimalsina <sudeeptimalsina18@gmail.com>
2026-08-05 14:41:52 +1000

    Minor changes

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -3,8 +3,8 @@ import { Button } from "@enterprise-webapp/ui";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to the Admin App</h1>
-      <Button>Create Project</Button>
+      <h1 className="text-4xl font-bold">Welcome to Admin App</h1>
+      <Button>Create Projects</Button>
     </main>
   );
 }
